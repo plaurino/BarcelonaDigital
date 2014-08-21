94193c05fb87a4191b267614b9451e16f5e4fc64
Pablo Terradillos <tehsis@gmail.com>
2014-08-21 01:05:55 -0300

    api to prod

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://localhost:3000',
+  host: 'http://104.131.243.228:8080/',
   headers: {
     Authorization: 'Basic ' + window.btoa(window.localStorage.getItem('user-token'))
   }

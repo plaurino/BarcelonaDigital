f8d77968c0049d122e0fa94291f3f822ec923559
Pablo Terradillos <tehsis@gmail.com>
2014-10-08 01:04:48 -0300

    moved sidebar and header to components

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isUserLogged: false,
-
+  needs: 'collections/main',
+  showSidebar: Ember.computed.alias('controllers.collections/main.showSidebar'),
   actions: {
+    toggleSidebar: function() {
+      this.toggleProperty('showSidebar');
+    },
     logout: function() {
       window.localStorage.removeItem('user-token');
       this.transitionToRoute('users.login');

05cbc047977938af844582ca92b92c7be3235c2a
omarteodoroalegre <alegreomarteodoro@gmail.com>
2015-01-23 03:43:09 -0300

    Zoom view and double page view updated.

--- app/components/kd-issue-page.js
+++ app/components/kd-issue-page.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNameBindings: ['selectedPage:show:hide'],
   classNames: ['issue-page'],
+  viewedPages: [],
   handleImageLoad: function() {
     var pageView = this;
     var image = pageView.$().children('img');
@@ -10,9 +11,6 @@ export default Ember.Component.extend({
     image.one('load', function() {
       Ember.run(function() {
         pageView.set('loaded', true);
-
-        //var height = (pageView.$(window).height() - 75);
-        //image.attr('height', height);
       });
     });
   }.on('didInsertElement'),
@@ -31,10 +29,30 @@ export default Ember.Component.extend({
         pageView.sendAction('prev');
       });
     });
+
+    hammer.on('doubletap', function() {
+      if(pageView.get('activatedZoom')){
+        Ember.run(function() {
+          pageView.sendAction('zoomOut', pageView.get('number'));
+        });
+      } else {
+        Ember.run(function() {
+          pageView.sendAction('zoomIn', pageView.get('number'));
+        });
+      }
+      console.log("double tap");
+    });
+
   }.on('didInsertElement'),
   selectedPage: function() {
-    var currentSelected = this.get('selected');
-    return currentSelected.contains(this.get('number'));
+    var pageView = this;
+    pageView.set('viewedPages', pageView.get('selected'));
+    if(pageView.get('activatedZoom')){
+      return pageView.get('selected') === pageView.get('number');
+    } else {
+      var currentSelected = pageView.get('selected');
+      return currentSelected.contains(pageView.get('number'));
+    }
   }.property('selected'),
   actions: {
     next: function() {
--- app/components/kd-issue.js
+++ app/components/kd-issue.js
@@ -5,85 +5,133 @@ export default Ember.Component.extend({
   activatedZoom: false,
   actions: {
     next: function() {
-      var view = this;
+      //var view = this;
       if(this.get('activatedZoom')){
-        Ember.run(function() {
-          view.send('zoomOut');
-        });
-      }
-
-      if (this.get('selected').contains((this.get('issues.pages').content.length-2)) && this.get('selected').length === 1) {
-        this.set('selected', [this.get('issues.pages').content.length-1]);
-      } else if (this.get('selected').contains((this.get('issues.pages').content.length-1))) {
-        this.set('selected', [0]);
+        //single page
+        if (this.get('selected') === this.get('issues.pages').content.length-1) {
+          this.set('selected', 0);
+        } else {
+          this.incrementProperty('selected');
+        }
       } else {
-        var currentPages = this.get('selected');
-        var pageEven = (currentPages[0]) ? currentPages[0] + 2 : 1;
-        var pageOdd = (currentPages[1]) ? currentPages[1] + 2 : 2;
+        // double page
+        if (this.get('selected').contains((this.get('issues.pages').content.length - 2)) && this.get('selected').length === 1) {
+          this.set('selected', [this.get('issues.pages').content.length - 1]);
+        } else if (this.get('selected').contains((this.get('issues.pages').content.length - 1))) {
+          this.set('selected', [0]);
+        } else {
+          var currentPages = this.get('selected');
+          var pageEven = (currentPages[0]) ? currentPages[0] + 2 : 1;
+          var pageOdd = (currentPages[1]) ? currentPages[1] + 2 : 2;
 
-        var selectedPages = [];
-        if(pageEven < (this.get('issues.pages').content.length-1)) {
-          selectedPages.push(pageEven);
-        }
-        if(pageOdd < (this.get('issues.pages').content.length-1)) {
-          selectedPages.push(pageOdd);
-        }
-        if(selectedPages.length === 0) {
-          selectedPages.push((this.get('issues.pages').content.length-1));
-        }
+          var selectedPages = [];
+          if (pageEven < (this.get('issues.pages').content.length - 1)) {
+            selectedPages.push(pageEven);
+          }
+          if (pageOdd < (this.get('issues.pages').content.length - 1)) {
+            selectedPages.push(pageOdd);
+          }
+          if (selectedPages.length === 0) {
+            selectedPages.push((this.get('issues.pages').content.length - 1));
+          }
 
-        this.set('selected', selectedPages);
+          this.set('selected', selectedPages);
+        }
       }
     },
     prev: function() {
-      var view = this;
+      //var view = this;
       if(this.get('activatedZoom')){
-        Ember.run(function() {
-          view.send('zoomOut');
-        });
-      }
-
-      if (this.get('selected').contains(0) ) {
-        this.set('selected', [this.get('issues.pages').content.length - 1]);
-      } else if (this.get('selected').contains(1) ) {
-        this.set('selected', [0]);
-      } else if (((this.get('issues.pages').content.length - 1) % 2 !== 0) && this.get('selected').contains((this.get('issues.pages').content.length-1))) {
-        this.set('selected', [this.get('issues.pages').content.length - 2, this.get('issues.pages').content.length - 3]);
-      } else if (((this.get('issues.pages').content.length - 1) % 2 === 0) && this.get('selected').contains((this.get('issues.pages').content.length-1))) {
-        this.set('selected', [this.get('issues.pages').content.length - 2]);
-      } else if (this.get('selected').contains((this.get('issues.pages').content.length-2)) && this.get('selected').length === 1) {
-        this.set('selected', [((this.get('issues.pages').content.length-2)-1), ((this.get('issues.pages').content.length-2)-2)]);
+        //single page
+        if (this.get('selected') === 0 ) {
+          this.set('selected', this.get('issues.pages').content.length-1);
+        } else {
+          this.decrementProperty('selected');
+        }
       } else {
-        var currentPages = this.get('selected');
-        var pageEven = (currentPages[0]) ? currentPages[0] - 2 : (this.get('issues.pages').content.length-1)-1;
-        var pageOdd = (currentPages[1]) ? currentPages[1] - 2 : (this.get('issues.pages').content.length-1)-2;
+        //double page
+        if (this.get('selected').contains(0)) {
+          this.set('selected', [this.get('issues.pages').content.length - 1]);
+        } else if (this.get('selected').contains(1)) {
+          this.set('selected', [0]);
+        } else if (((this.get('issues.pages').content.length - 1) % 2 !== 0) && this.get('selected').contains((this.get('issues.pages').content.length - 1))) {
+          this.set('selected', [this.get('issues.pages').content.length - 2, this.get('issues.pages').content.length - 3]);
+        } else if (((this.get('issues.pages').content.length - 1) % 2 === 0) && this.get('selected').contains((this.get('issues.pages').content.length - 1))) {
+          this.set('selected', [this.get('issues.pages').content.length - 2]);
+        } else if (this.get('selected').contains((this.get('issues.pages').content.length - 2)) && this.get('selected').length === 1) {
+          this.set('selected', [((this.get('issues.pages').content.length - 2) - 1), ((this.get('issues.pages').content.length - 2) - 2)]);
+        } else {
+          var currentPages = this.get('selected');
+          var pageEven = (currentPages[0]) ? currentPages[0] - 2 : (this.get('issues.pages').content.length - 1) - 1;
+          var pageOdd = (currentPages[1]) ? currentPages[1] - 2 : (this.get('issues.pages').content.length - 1) - 2;
 
-        var selectedPages = [];
-        if(pageEven > 0) {
-          selectedPages.push(pageEven);
-        }
-        if(pageOdd > 0) {
-          selectedPages.push(pageOdd);
-        }
+          var selectedPages = [];
+          if (pageEven > 0) {
+            selectedPages.push(pageEven);
+          }
+          if (pageOdd > 0) {
+            selectedPages.push(pageOdd);
+          }
 
-        this.set('selected', selectedPages);
+          this.set('selected', selectedPages);
+        }
       }
     },
-    zoomIn: function(){
-      this.set('activatedZoom', true);
-
+    zoomIn: function(page){
       var view = this;
-      var image = view.$('.issue-page.show');
+
+      if(page == undefined && view.get('viewedPages') instanceof Array) {
+        page = view.get('viewedPages')[0];
+      } else if(page == undefined && !(view.get('viewedPages') instanceof Array) && view.get('viewedPages') != undefined) {
+        page = view.get('viewedPages');
+      }
+
+      this.set('activatedZoom', true);
+      this.set('selected', page);
+
+      var image = view.$('.issue-page');
       image.css('width', '100%');
-      //console.log('activated zoom');
+      console.log('activated zoom in page number ' + this.get('selected'));
+
+      var imageS = view.$('.issue-page.show');
+      console.log(imageS);
     },
-    zoomOut: function(){
+    zoomOut: function(page){
+      var view = this;
+
+      if(page == undefined && view.get('viewedPages') instanceof Array) {
+        page = view.get('viewedPages')[0];
+      } else if(page == undefined && !(view.get('viewedPages') instanceof Array) && view.get('viewedPages') != undefined) {
+        page = view.get('viewedPages');
+      }
+
       this.set('activatedZoom', false);
 
-      var view = this;
-      var image = view.$('.issue-page.show');
+      var selectedPages = [];
+
+      if(page % 2 == 0 && page != 0){
+        if (page-1 != 0 || page-1 != (this.get('issues.pages').content.length - 1)) {
+          selectedPages.push(page-1);
+        }
+      }
+      if (page != 0 || page != (this.get('issues.pages').content.length - 1)) {
+        selectedPages.push(page);
+      }
+      if(page % 2 != 0){
+        if (page+1 != 0 || page+1 != (this.get('issues.pages').content.length - 1)) {
+          selectedPages.push(page+1);
+        }
+      }
+
+      if(selectedPages.length > 0) {
+        this.set('selected', selectedPages);
+      } else {
+        this.set('selected', page);
+      }
+
+      var image = view.$('.issue-page');
       image.css('width', '50%');
-      //console.log('unactivated zoom');
+      console.log('unactivated zoom in page number ' + this.get('selected'));
     }
   }
 });
